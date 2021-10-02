c876f571cbd3bae530a3ef64807eb6ad2b4daa3a
tarunjain3 <tarun9041682875@gmail.com>
2021-10-02 15:29:35 +0530

    colVal

---
+++ src/components/HomePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container" style="float: right">
+    <div class="row">
+      <div class="col-lg-1">
+        <p>1</p>
+      </div>
+      <div v-for="index in 10" :key="index.id">
+        <div class="col-lg-1">
+          <div class="ageSpan-deact"></div>
+        </div>
+      </div>
+      <div class="col-lg-1 mar">100</div>
+    </div>
+  </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      objects: [
+        { name: "abc", age: 10 },
+        { name: "def", age: 15 },
+        { name: "xyz", age: 15 },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.ageSpan-act {
+  height: 16px;
+  width: 16px;
+  background-color: yellow;
+}
+.ageSpan-deact {
+  height: 8px;
+  width: 8px;
+  background-color: grey;
+}
+</style>
